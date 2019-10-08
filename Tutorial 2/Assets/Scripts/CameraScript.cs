2959fedc3aabe0941b5293d852ce101006d93782
geistpolter <54725759+geistpolter@users.noreply.github.com>
2019-10-08 18:41:14 -0400

    Tutorial2
    By Cheyenne Fiedler

---
+++ Tutorial 2/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    //FixedUpdate for Physics
+    //LateUpdate is when you want the code to be the last thing to happen
+    void LateUpdate()
+    {
+        this.transform.position = new Vector3(target.transform.position.x, this.transform.position.y, this.transform.position.z);
+    }
+}
---
+++ Tutorial 2/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+ //Remember to change the name of the public class script if you rename script in unity or else the code won't work.  The object has to match the name of the file.
+{
+    private Rigidbody2D rd2d;
+    public float speed;
+    public Text score;
+    private int scoreValue = 0;
+    //How things are at beginning of project and are the source of things interacted with
+    void Start()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+        score.text = scoreValue.ToString();
+    }
+
+    //Code for things that change over course of project's execution and when you want to interact with things in game.
+    void FixedUpdate()
+    {
+        float hozMovement = Input.GetAxis("Horizontal");
+        float verMovement = Input.GetAxis("Vertical");
+
+        rd2d.AddForce(new Vector2(hozMovement * speed, verMovement * speed));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Coin")
+        {
+            scoreValue += 1;
+            score.text = scoreValue.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Ground")
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+            }
+        }
+    }
+}
